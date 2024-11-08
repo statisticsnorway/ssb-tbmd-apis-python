28129b650dc014feb0e64f6f299645cdf3b0c69b
Corneil, Carl Franklin <Carl.Corneil@ssb.no>
2024-11-08 09:24:59 +0100

    adding possibility to build paths that datadok uses from oracle sql query

---
+++ src/ssb_tbmd_apis/oracle_direct/oracle_paths.py
@@ -0,0 +1,91 @@
+import getpass
+import pandas as pd
+import oracledb
+
+
+def paths_in_substamme(stamme_substamme: list[tuple[str, str]] | tuple[str, str] | str, database: str) -> list[str]:
+    """Try to recreate the paths used by Datadok under a stamme and substamme.
+    
+    Requires that you have a user in the correct database, and that it has the datadok-read-role. 
+    The password for the database is also different from your usual password.
+    
+    Args:
+        stamme_substamme: A single, or multiple linux-stammes you want to find paths in. 
+            Can be slash-seperated string, a tuple with two elements (stamme and substamme).
+            Or it can be a list of tuples, where the tuples contain two string (stamme and substamme),
+            allowing to get for many substammes at the same time (only enter database password once).
+        database: The name of the database containing the datadok-data, and lots of other data at Statistics Norway.
+        
+    Returns:
+        list[str]: The paths constructed from the contents in the database.
+    
+    Raises:
+        error: If the fetching from database doesnt work out.
+    
+    """
+    # Support different informats by wrapping simple types in iterators
+    if isinstance(stamme_substamme, str):
+        stamme_substamme = tuple(stamme_substamme.split("/"))
+    if isinstance(stamme_substamme, tuple):
+        stamme_substamme = [stamme_substamme]
+    
+    
+    union_queries = []
+    for stamme, substamme in stamme_substamme_pairs:
+        union_queries.append(f"""
+            SELECT 
+                '${stamme.upper()}/{substamme}/arkiv/' || f.filklasse_navn || '/' || g.filnavn AS full_path
+            FROM (
+                SELECT filnivaa_id AS filklasse_id, filnivaa_navn AS filklasse_navn, filnivaa_filnivaa_id AS substamme_id
+                FROM DATADOK.FILNIVAA
+                WHERE filnivaa_nivaa = 'Filklasse'
+                  AND filnivaa_filnivaa_id = (
+                      SELECT filnivaa_id 
+                      FROM DATADOK.FILNIVAA
+                      WHERE LOWER(filnivaa_navn) = LOWER('{substamme}')
+                        AND filnivaa_nivaa = 'Substamme'
+                        AND filnivaa_filnivaa_id = (
+                            SELECT filnivaa_id 
+                            FROM DATADOK.FILNIVAA
+                            WHERE LOWER(filnivaa_navn) = LOWER('{stamme}')
+                              AND filnivaa_nivaa = 'Stamme'
+                        )
+                  )
+            ) f
+            JOIN (
+                SELECT 
+                    filnivaa_navn, 
+                    CASE 
+                        WHEN filnivaa_datatype IS NOT NULL AND filnivaa_navn != filnivaa_datatype 
+                        THEN filnivaa_navn || filnivaa_datatype 
+                        ELSE filnivaa_navn 
+                    END AS filnavn,
+                    filnivaa_filnivaa_id AS filklasse_id
+                FROM DATADOK.FILNIVAA
+                WHERE filnivaa_nivaa = 'Generasjon'
+            ) g
+            ON f.filklasse_id = g.filklasse_id
+        """)
+
+    # Combine the individual queries with UNION ALL
+    full_query = " UNION ALL ".join(union_queries)
+    
+    results = []
+    user = getpass.getuser()
+    try:
+        with oracledb.connect(user=user,
+                             password=getpass.getpass(f"Oracle password for {user}: "),
+                             dsn=database) as conn:
+            with conn.cursor() as cur:
+                # execute the select SQL query
+                cur.execute(full_query)
+                # gets all the data in batches
+                while True:
+                    rows = cur.fetchmany(1000)
+                    if not rows:
+                        break
+                    else:
+                        results += [x[0] for x in rows]
+    except oracledb.Error as error:
+        raise error
+    return results
